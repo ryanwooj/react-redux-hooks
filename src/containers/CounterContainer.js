e09aac6194df956d7af03a38a64298e99d95afff
Ryan Woo <devrwoo@gmail.com>
2019-04-23 13:41:46 -0700

    useActions 는 뭘까...

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -7,7 +7,7 @@ import {increment, decrement} from '../modules/counter';
 const CounterContainer = () => {
   //카운터 겂을 스테이트 안의 state.counter값 안 어레이로 지정한다.
     const counter = useSelector(state => state.counter, []);
-
+    //올리고 내리고에 인크리먼트, 디크리먼트라는 액션을 사용한다. 아직 이해못함.
     const [onIncrease, onDecrease] = useActions([increment, decrement], []);
   return (
    <Counter number={counter} onIncrease={onIncrease} onDecrease={onDecrease} />
