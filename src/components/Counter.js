967bc4054c284c42d17ee21ab6b2a43522f8b859
Ryan Woo <devrwoo@gmail.com>
2019-04-24 13:46:09 -0700

    number use

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,7 @@ import React from 'react'
 const Counter = ({ onIncrease, onDecrease, number }) => {
   return (
     <div>
+    // 현재 스테이트 안의 넘버라는 상수 값. 
       <h1>{number}</h1>
         <div>
             <button onClick={onIncrease}>+1</button>
